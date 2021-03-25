b5e784b8f3e567c739d1fafe6d56b62ebb2134b0
egee_irl <brian@egee.io>
2021-03-25 12:17:39 -0700

    flatpaks

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -11,6 +11,26 @@ WORKDIR="/tmp/distrodelves-bootstrap"
 mkdir -p $WORKDIR
 cd $WORKDIR || exit
 
+install_mangohud() {
+	echo "INFO: installing MangoHud"
+	curl -L https://github.com/flightlessmango/MangoHud/releases/download/v0.6.1/MangoHud-0.6.1.tar.gz -o $WORKDIR/MangoHud.tar.gz
+	tar -xf $WORKDIR/MangoHud.tar.gz
+	bash $WORKDIR/MangoHud/mangohud-setup.sh install
+}
+
+install_flatpaks () {
+	echo "INFO: installing Flatpaks, this will take a while..."
+	pacman -S flatpak 
+	flatpak install flathub com.leinardi.gst -y
+	flatpak install flathub com.basemark.BasemarkGPU -y
+	flatpak install flathub io.github.arunsivaramanneo.GPUViewer -y
+
+	flatpak install flathub org.xonotic.Xonotic -y
+	flatpak install flathub org.zdoom.GZDoom -y
+	flatpak install flathub io.github.freedoom.Phase1 -y
+	flatpak install flathub com.moddb.TotalChaos -y
+}
+
 # Arch-based
 if [ "$family" = "arch" ]; then
 	echo "INFO: enabling multilib & forcing resync for multilib"
@@ -19,7 +39,7 @@ if [ "$family" = "arch" ]; then
 	
 	echo "INFO: updating system & installing packages"
 	sudo pacman -Syyu
-	sudo pacman -S lib32-mesa lib32-vulkan-icd-loader git curl base-devel vulkan-icd-loader flatpak steam lutris wine
+	sudo pacman -S lib32-mesa lib32-vulkan-icd-loader git curl base-devel vulkan-icd-loader steam lutris wine
 	if [ -z "$(which yay)" ]; then
 		echo "INFO: installing Yay"
 		git clone https://aur.archlinux.org/yay-bin.git
@@ -29,10 +49,8 @@ if [ "$family" = "arch" ]; then
 	fi
 	#echo "INFO: installing AUR packages"
 	
-	echo "INFO: installing MangoHud"
-	curl -L https://github.com/flightlessmango/MangoHud/releases/download/v0.6.1/MangoHud-0.6.1.tar.gz -o $WORKDIR/MangoHud.tar.gz
-	tar -xf $WORKDIR/MangoHud.tar.gz
-	bash $WORKDIR/MangoHud/mangohud-setup.sh install
+	install_mangohud
+	install_flatpaks
 
 	echo "Install zen kernel? [y/N]"
 	read -r zen
