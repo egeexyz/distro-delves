05a8d0415798ce7014ddd24fc1f90f2fed44547f
advaithm <advaith.madhukar@gmail.com>
2021-03-25 22:26:09 +0530

    added support for debian/ubuntu and fedora

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -10,11 +10,29 @@ if [ -f /usr/bin/pacman ]; then
 		git clone https://aur.archlinux.org/yay-bin.git
 		cd yay-bin || exit
 		makepkg -si
-		yay -S mangohud # TODO: figure out which version of mangohud is the best for each system
+		yay -S mangohud 
 		echo "Install zen kernel? [Y/N]"
 		read -r zen
 		if [ "$zen" == "Y" ]; then
 				sudo pacman -S linux-zen
 		fi
+# ubuntu/debian
+elif [ -f /usr/bin/apt ]; then
+		on_ubuntu=$(lsb_release -i)
+		if [ "$on_ubuntu" == "Distributor ID: Ubuntu" ]; then
+				apt update && apt upgrade
+				apt install lutris -y
+		else 
+				echo "deb http://download.opensuse.org/repositories/home:/strycore/Debian_10/ ./" | sudo tee /etc/apt/sources.list.d/lutris.list
+				wget -q https://download.opensuse.org/repositories/home:/strycore/Debian_10/Release.key -O- | sudo apt-key add -
+				sudo apt update
+				sudo apt upgrade
+		fi
+# fedora/Mageia 8
+elif [ -f /usr/bin/dnf ]; then
+		dnf upgrade && dnf install lutris -y
+else
+		echo "Unsuported linux distribution, considering porting this script to your distro and opening a pr"
 fi
-		
+
+# TODO opensuse support
