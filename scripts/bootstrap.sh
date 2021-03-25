857150fcc2f0bad2c30e725b23215151e39476ff
egee_irl <brian@egee.io>
2021-03-25 11:22:46 -0700

    comments to echos

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -8,17 +8,22 @@ fi
 
 # Arch-based
 if [ "$family" = "arch" ]; then
-	# enable multilib
+	echo "INFO: enabling multilib & forcing resync for multilib"
 	sudo sed -i 's/#[multilib]/[multilib]/g' /etc/pacman.conf
 	sudo sed -i 's/#Include = \/etc\/pacman.d\/mirrorlist\//Include = \/etc\/pacman.d\/mirrorlist\//g' /etc/pacman.conf
-	sudo pacman -Syyu # force resync for multilib
+	
+	echo "INFO: updating system & installing packages"
+	sudo pacman -Syyu
 	sudo pacman -S base-devel git lib32-mesa lib32-vulkan-icd-loader vulkan-icd-loader steam lutris wine
 	if [ -z "$(which yay)" ]; then
+		echo "INFO: installing Yay"
 		git clone https://aur.archlinux.org/yay-bin.git
 		cd yay-bin || exit
 		makepkg -si
 	fi
+	echo "INFO: installing AUR packages"
 	yay -S mangohud # TODO: figure out which version of mangohud is the best for each system
+
 	echo "Install zen kernel? [y/N]"
 	read -r zen
 	if [ "$zen" == "y" ]; then sudo pacman -S linux-zen; fi
