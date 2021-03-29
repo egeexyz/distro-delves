611f79dafb182a2d80a184f0ce11ade8aa648c55
egee_irl <brian@egee.io>
2021-03-28 19:10:32 -0700

    locate pkgmgrs with which

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,7 +5,7 @@ source "./shared.sh"
 install_brew
 
 # Arch-based
-if [[ -n "$(which pacman)" ]]; then
+if [ -n "$(which pacman)" ]; then
 	echo "INFO: enabling multilib & forcing resync for multilib"
 	sudo sed -i 's/#[multilib]/[multilib]/g' /etc/pacman.conf
 	sudo sed -i 's/#Include = \/etc\/pacman.d\/mirrorlist\//Include = \/etc\/pacman.d\/mirrorlist\//g' /etc/pacman.conf
@@ -24,33 +24,33 @@ if [[ -n "$(which pacman)" ]]; then
 	read -r zen
 	if [ "$zen" == "y" ]; then sudo pacman -S --noconfirm linux-zen; fi
 # ubuntu/debian
-elif [ -f /usr/bin/apt ]; then
+elif [ -n "$(which apt-get)" ]; then
 		echo "INFO: enabling multilib "
 		dpkg --add-architecture i386
 		echo "INFO: updating system & installing packages"
-		sudo apt update
-		sudo apt upgrade
-		sudo apt install -y  mesa-vulkan-drivers flatpak curl libvulkan1 vulkan-utils mesa mesa:i386 wine
+		sudo apt-get update
+		sudo apt-get upgrade -y
+		sudo apt-get install -y mesa-vulkan-drivers flatpak curl libvulkan1 vulkan-utils mesa mesa:i386 wine
 		on_ubuntu=$(lsb_release -i)
 		if [ "$on_ubuntu" == "Distributor ID: Ubuntu" ]; then
-				apt install lutris -y
+			apt install lutris -y
 		else 
-				echo "INFO: on debian getting lutris from OBS"
-				echo "deb http://download.opensuse.org/repositories/home:/strycore/Debian_10/ ./" | sudo tee /etc/apt/sources.list.d/lutris.list
-				wget -q https://download.opensuse.org/repositories/home:/strycore/Debian_10/Release.key -O- | sudo apt-key add -
-				sudo apt update
-				sudo apt upgrade
-				sudo apt install lurtis
+			echo "INFO: on debian getting lutris from OBS"
+			echo "deb http://download.opensuse.org/repositories/home:/strycore/Debian_10/ ./" | sudo tee /etc/apt/sources.list.d/lutris.list
+			wget -q https://download.opensuse.org/repositories/home:/strycore/Debian_10/Release.key -O- | sudo apt-key add -
+			sudo apt-get update
+			sudo apt-get upgrade -y
+			sudo apt-get install -y lutris
 		fi
 # fedora/Mageia 8
-elif [ -f /usr/bin/dnf ]; then
+elif [ -n "$(which dnf)" ]; then
 		echo "INFO: adding rpm fusion repos"
 		sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 		echo "INFO: updating system & installing packages"
 		sudo dnf upgrade
 		sudo dnf install flatpak vulkan-loader.i686 curl vulkan-loader.x86_64 mesa.i686 mesa steam	 -y
 #opensuse 
-elif [ -f /usr/bin/zypper ]; then
+elif [ -n "$(which zypper)" ]; then
 		echo "INFO: updating system & installing packages"
 		sudo zypper install flatpak vulkan-loader wine curl steam -y 
 else
