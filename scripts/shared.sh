ba932fa7dde8b392a06f9835661e576c29ebd3e4
egee_irl <brian@egee.io>
2021-03-28 21:33:51 -0700

    typo

--- scripts/shared.sh
+++ scripts/shared.sh
@@ -31,6 +31,7 @@ install_flatpaks() {
 install_phoronix() {
 	brew install gcc
 	brew install php
+	brew install make
     brew install phoronix-test-suite
     yes y | phoronix-test-suite
 
@@ -39,5 +40,5 @@ install_phoronix() {
 	curl -L https://gist.githubusercontent.com/egee-irl/5265d9a5e44e9d14dee175be5a39ce63/raw/7b4435c5a91492614d7438bf3abcdf93eb0bec85/user-config.xml \
 		-o "$HOME/.phoronix-test-suite/user-config.xml"
 	curl -L https://gist.githubusercontent.com/egee-irl/d08076d660e9275ae23d8e3c9b6ca62d/raw/4300163d25f84f607cb7cba36cacfdefa0926ce0/eg-basic \
-		-o "$HOME/.phoronix-test-suite/text-suites/local/eg-basic/suite-definition.xml"
+		-o "$HOME/.phoronix-test-suite/test-suites/local/eg-basic/suite-definition.xml"
 }
