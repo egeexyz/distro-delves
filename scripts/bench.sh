ddfe0a0f9c9b8ea1130dce51a3080ff1c3c8d014
egee_irl <brian@egee.io>
2021-04-02 21:31:12 -0700

    remove forking apps bit

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -12,8 +12,8 @@ compression=("pts/compress-7zip-1.7.1" "pts/compress-gzip-1.2.0" "pts/compress-p
 devel=("pts/pybench-1.1.3" "pts/phpbench-1.1.6" "pts/rbenchmark-1.0.3" "pts/node-octane-1.0.1")
 encryption=("pts/blake2-1.2.1" "pts/gcrypt-1.1.2" "pts/gnupg-2.5.0")
 gpu_perf=("pts/betsy-1.0.0" "pts/gputest-1.3.2")
-gpu_games_oss=("pts/xonotic-1.5.1" "pts/tesseract-1.1.0" "pts/supertuxkart-1.6.0" "pts/xplane11-1.1.2")
-gpu_games_steam=("pts/csgo-1.6.0" "pts/tf2-1.2.3" "pts/dota2-1.2.6" "pts/portal-1.1.2")
+games_oss=("pts/xonotic-1.5.1" "pts/tesseract-1.1.0" "pts/supertuxkart-1.6.0" "pts/xplane11-1.1.2")
+games_steam=("pts/csgo-1.6.0" "pts/tf2-1.2.3" "pts/dota2-1.2.6" "pts/portal-1.1.2")
 all_of_em=( "${disk[@]}" "${os[@]}"  "${ram[@]}" "${compiling[@]}" "${encoding[@]}" "${compression[@]}" "${devel[@]}" "${encryption[@]}" "${gpu_perf[@]}" "${gpu_games_oss[@]}" "${gpu_games_steam[@]}" )
 
 install_tests() {
