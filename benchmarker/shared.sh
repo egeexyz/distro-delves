2e180b56f89915db9c79a2915da30a3130e77758
egee-irl <brian@egee.io>
2021-07-19 19:16:47 -0700

    bump version

--- benchmarker/shared.sh
+++ benchmarker/shared.sh
@@ -3,7 +3,7 @@ set -eu
 
 install_mangohud() {
 	echo "INFO: installing MangoHud"
-	curl -L https://github.com/flightlessmango/MangoHud/releases/download/v0.6.1/MangoHud-0.6.1.tar.gz -o /tmp/MangoHud.tar.gz
+	curl -L https://github.com/flightlessmango/MangoHud/releases/download/v0.6.5/MangoHud-0.6.5.tar.gz -o /tmp/MangoHud.tar.gz
 	tar -C /tmp/ -xf /tmp/MangoHud.tar.gz
 	bash /tmp/MangoHud/mangohud-setup.sh install
 }
