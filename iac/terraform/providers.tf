25d3bc7eba3dde7feb253ca93758ab38a74e6cb8
egee-irl <brian@egee.io>
2022-05-20 05:56:40 -0700

    remove unused providers

--- iac/terraform/providers.tf
+++ iac/terraform/providers.tf
@@ -1,18 +1,16 @@
 terraform {
+  backend "s3" {
+    bucket = "eg-tfstate"
+    key    = "delve.tfstate"
+    region = "us-west-2"
+  }
   required_providers {
     aws = {
       source = "hashicorp/aws"
     }
   }
 }
+
 provider "aws" {
   region = "us-west-2"
-}
-
-terraform {
-  backend "s3" {
-    bucket = "eg-tfstate"
-    key    = "delve.tfstate"
-    region = "us-west-2"
-  }
 }
